6f7a07a8c9634215de7a4b4086c25da2c27da461
Shubham Rawat <shubham.rawat@ignitarium.com>
2024-09-20 17:48:18 +0530

    add edit dialog in groups

--- src/components/organisms/Groups/index.tsx
+++ src/components/organisms/Groups/index.tsx
@@ -1,6 +1,17 @@
+import { DialogEdit } from "@/components/organisms/Groups/DialogEdit";
 import { TableCustom } from "@/components/molecules/TableCustom";
+import { useState } from "react";
+
+export interface IGroups {
+  id: string;
+  name: string;
+  link: string;
+}
 
 export default function Groups() {
+  const [openEdit, setOpenEdit] = useState(false);
+  const [editData, setEditData] = useState<IGroups | null>(null);
+
   const columnsName = [
     {
       name: "ID",
@@ -13,33 +24,37 @@ export default function Groups() {
   const columnsData = [
     {
       id: "1",
-      name: "COMMUNITY",
+      name: "COMMUNITY1",
       link: "https://chat.whatsapp.com/J98k3LCByjC4GzwwxvZOBJ",
     },
     {
       id: "2",
-      name: "COMMUNITY",
+      name: "COMMUNITY2",
       link: "https://chat.whatsapp.com/J98k3LCByjC4GzwwxvZOBJ",
     },
     {
       id: "3",
-      name: "COMMUNITY",
+      name: "COMMUNITY3",
       link: "https://chat.whatsapp.com/J98k3LCByjC4GzwwxvZOBJ",
     },
     {
       id: "4",
-      name: "COMMUNITY",
+      name: "COMMUNITY4",
       link: "https://chat.whatsapp.com/J98k3LCByjC4GzwwxvZOBJ",
     },
     {
       id: "5",
-      name: "COMMUNITY",
+      name: "COMMUNITY5",
       link: "https://chat.whatsapp.com/J98k3LCByjC4GzwwxvZOBJ",
     },
   ];
 
   const handleEdit = (id: string) => {
     console.log(`Edit item with ID: ${id}`);
+    setOpenEdit(true);
+
+    const data = columnsData.find((column) => column.id === id);
+    if (data) setEditData(data);
   };
 
   return (
@@ -50,6 +65,13 @@ export default function Groups() {
         onEdit={handleEdit}
         showEdit={true}
       />
+      {openEdit && (
+        <DialogEdit
+          open={openEdit}
+          setOpen={() => setOpenEdit(false)}
+          editData={editData}
+        />
+      )}
     </div>
   );
 }
